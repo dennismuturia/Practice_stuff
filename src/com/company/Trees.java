d2e1613fc920e50ea503273c8f2bddacef246f3b
dennis <dmuturia2922@gmail.com>
2018-12-27 16:30:42 +0300

    I have created a tree class that contains an abstract class of Node to store all the djacent node references. This willbethe correct structure . The simpletree class is  just a simple way to implement the binary search tee

---
+++ src/com/company/Trees.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Trees {
+    public abstract class Node{
+        private Node[] children;
+
+        public Node(Node[] children){
+            this.children = children;
+        }
+        public int getNumChild(){
+            return children.length;
+        }
+        public Node getChild(int index){
+            return children[index];
+        }
+    }
+    public class IntNode extends Node{
+        private int value;
+
+        public IntNode(Node[] children, int value){
+            super(children);
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
