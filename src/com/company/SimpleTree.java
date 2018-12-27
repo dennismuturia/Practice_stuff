6fef50761de5a1d76a06fdc905037682a53cb801
dennis <dmuturia2922@gmail.com>
2018-12-27 16:37:28 +0300

    IN the simple tree created a Model for the Node with basic getters for a binary search tree.

--- src/com/company/SimpleTree.java
+++ src/com/company/SimpleTree.java
@@ -1,4 +1,29 @@
 package com.company;
 
 public class SimpleTree {
+    public class Node{
+        //FIrst create some privatye global variables to avoid external manipulation
+        private Node left;
+        private Node right;
+        private int V;
+
+        public Node(Node left, Node right, int V){
+            this.left = left;
+            this.right = right;
+            this.V = V;
+        }
+        //Create Getters in this method
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public int getV() {
+            return V;
+        }
+    }
 }
